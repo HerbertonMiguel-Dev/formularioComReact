72df0b95d824b1a6868468f74f27b5e823338d0f
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-29 15:35:24 -0300

    manipulando formularios com React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, {Component} from "react";
-import Feed from "./components/Feed";
 
 
 class App extends Component{
   
-  constructor(props){
-    super(props);
-    this.state = {
-      feed:[
-        {id: 1, username: 'Herberton', curtidas: 10, comentarios: 2},
-        {id: 2, username: 'Miguel', curtidas: 11, comentarios: 3},
-        {id: 3, username: 'Rafael', curtidas: 12, comentarios: 4},
-        {id: 4, username: 'Carol', curtidas: 1, comentarios: 1},
-      ]
-    };
-  }
 
   render(){
     return(
       <div>
-        {this.state.feed.map((item)=>{
-          return(
-            <Feed key={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
-          )
-        })}
+        <h2>Login</h2>
+        Email:
+        <input type="email" name="email" value="teste@teste.com"/> 
+        <br/>
+        senha:
+        <input type="password" name="senha" value="123123"/>
       </div>
     )
   }
